f642a1a59d6a678a9b025d09ca90fe6a0e4aac59
Josh <64910295+Arctic4161@users.noreply.github.com>
2024-08-01 12:15:25 -0400

    Move variables to extract audio

--- audio_extract/ffmpeg.py
+++ audio_extract/ffmpeg.py
@@ -2,8 +2,6 @@
 import subprocess
 import platform
 from kivy import platform
-android_local = False
-duration_local = False
 
 # Local modules
 from audio_extract.validators import AudioExtractValidator
@@ -11,9 +9,7 @@ from audio_extract.validators import AudioExtractValidator
 if platform != "android":
     import imageio_ffmpeg
     FFMPEG_BINARY = imageio_ffmpeg.get_ffmpeg_exe()
-    print(FFMPEG_BINARY)
 else:
-    android_local = True
     from jnius import autoclass
     from jnius import *
     FFMPEG_BINARY = autoclass('com.sahib.pyff.ffpy')
@@ -24,6 +20,10 @@ def extract_audio(input_path: str, output_path: str = "./audio.mp3", output_form
                   duration: float = None,
                   overwrite: bool = False):
     validator = AudioExtractValidator(input_path, output_path, output_format, duration, start_time, overwrite)
+    if platform != "android":
+        android_local = False
+    else:
+        android_local = True
     result = validator.validate()
 
     cleaned_input_path = result["input_path"]
@@ -31,7 +31,6 @@ def extract_audio(input_path: str, output_path: str = "./audio.mp3", output_form
     cleaned_output_format = result["output_format"]
     cleaned_start_time = result["start_time"]
     if android_local is False:
-        print(android_local)
         command = [FFMPEG_BINARY,
                    '-i', cleaned_input_path,
                    '-ss', cleaned_start_time,
@@ -42,6 +41,8 @@ def extract_audio(input_path: str, output_path: str = "./audio.mp3", output_form
         if android_local is False:
             command.insert(3, "-t")
             command.insert(4, cleaned_duration)
+    else:
+        duration_local = False
 
     if platform == "win":
         si = subprocess.STARTUPINFO()
@@ -58,6 +59,6 @@ def extract_audio(input_path: str, output_path: str = "./audio.mp3", output_form
                 f'-i {cleaned_input_path} -ss {cleaned_start_time} -t {cleaned_duration} -f {cleaned_output_format} -y {cleaned_output_path}'))
 
     if result.returncode == 0:
-        print(f"Success : audio file has been saved to \"{cleaned_output_path}\".")
+        retur f"Success : audio file has been saved to \"{cleaned_output_path}\"."
     error = result.stderr.decode().strip().split("\n")[-1]
     return f"Failed : {error}."
